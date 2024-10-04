710309190d985dd192d779dfef090e649a20f977
Jati Rahmatulloh <145977090+Jampaaang@users.noreply.github.com>
2024-10-04 11:37:49 +0700

    Add files via upload

---
+++ Tugas5/mvc/views/home/index.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/assets/style2.css">
+<h1>Selamat Datang di Aplikasi Penjualan Barang</h1>
+<p>Ini adalah halaman home.</p>
+<a href="<?= BASEURL; ?>index.php?url=login/index">Login</a>
---
+++ Tugas5/mvc/views/login/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/assets/style1.css">
+</head>
+<body>
+    <h2>Form Login</h2>
+    <form action="<?= BASEURL; ?>index.php?url=login/auth" method="POST">
+        <input type="text" name="username" placeholder="Username" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
